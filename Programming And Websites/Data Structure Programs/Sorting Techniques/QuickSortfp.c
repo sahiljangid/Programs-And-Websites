e2c930322e819752881e40ab37619ac7fdc56c8e
sahiljangid <57535917+sahiljangid@users.noreply.github.com>
2022-10-11 22:10:36 +0530

    update quick sort

--- Programming And Websites/Data Structure Programs/Sorting Techniques/QuickSortfp.c
+++ Programming And Websites/Data Structure Programs/Sorting Techniques/QuickSortfp.c
@@ -1,59 +1,69 @@
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<conio.h>
 
-int Partition(int arr[], int s, int e)
+int Partition(int arr[], int first, int last)
 {
-    int pivot = arr[s];
-    int pindex = s+1;
-
-    for (int i = s+1; i <= e; i++)
+    int x = arr[first];
+    int i = first;
+    for (int j = first+1; j <= last; j++)
     {
-        if (arr[i] < pivot)
+        if (arr[j] <= x)
         {
-            int temp;
-            temp = arr[i];
-            arr[i] = arr[pindex];
-            arr[pindex] = temp;
-            pindex++;
+            i = i + 1;
+			int temp;
+			temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
         }
     }
     int temp;
-    temp = arr[s];
-    arr[s] = arr[pindex-1];
-    arr[pindex-1] = temp;
-
-    return pindex-1;
+    temp = arr[i];
+    arr[i] = arr[first];
+    arr[first] = temp;
+    return i; 
+    
 }
 
-void QuickSort(int arr[], int s, int e)
+
+
+void QuickSort(int arr[], int first, int last)
 {
-    if (s < e)
+    if (first<last)
     {
-        int p = Partition(arr, s, e);
-        QuickSort(arr, s, p - 1);
-        QuickSort(arr, p + 1, e);
+        int p = Partition(arr,first,last);
+        QuickSort(arr,first,p-1);
+        QuickSort(arr,p+1,last);
     }
+    
 }
+
+
+
+
 
 int main()
 {
     int size;
-    printf("Enter the size of array: ");
-    scanf("%d", &size);
+    printf("Enter the size of array : ");
+    scanf("%d",&size);
     int arr[size];
-    printf("Enter %d elements one by one : \n", size);
+    printf("Enter %d Elements one by one: \n",size);
     for (int i = 0; i < size; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d",&arr[i]);
     }
-
-    QuickSort(arr, 0, size-1);
-
+    
+    printf("Your Entered Array Is: ");
     for (int i = 0; i < size; i++)
     {
-        printf("%d  ", arr[i]);
+        printf("%d ",arr[i]);
     }
 
-    return 0;
+    QuickSort(arr,0,size-1);
+
+    printf("\n\nYour Sorted Array Is: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }    
 }
